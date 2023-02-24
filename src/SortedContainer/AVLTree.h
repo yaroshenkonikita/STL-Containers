433f69686027b7c1cfd96362cfdff3a3fdc009da
Nikita Yaroshenko <yaroshenkonikita4@gmail.com>
2023-02-24 12:36:38 +0300

    class template AVLTree

---
+++ src/SortedContainer/AVLTree.h
@@ -0,0 +1,17 @@
+//
+// Created by 12355 on 24.02.2023.
+//
+
+#ifndef CPP2_S21_CONTAINERS_0_AVLTREE_H
+#define CPP2_S21_CONTAINERS_0_AVLTREE_H
+
+#include "Node.h"
+
+
+template <typename Key, typename T>
+class AVLTree {
+    Node *root;
+};
+
+
+#endif //CPP2_S21_CONTAINERS_0_AVLTREE_H
