e53861ba7cd3468806cd23088a1e20e29fea4a9d
Nikita Yaroshenko <yaroshenkonikita4@gmail.com>
2023-02-24 12:46:05 +0300

    rework

--- src/SortedContainer/AVLTree.h
+++ src/SortedContainer/AVLTree.h
@@ -5,12 +5,27 @@
 #ifndef CPP2_S21_CONTAINERS_0_AVLTREE_H
 #define CPP2_S21_CONTAINERS_0_AVLTREE_H
 
-#include "Node.h"
-
-
 template <typename Key, typename T>
 class AVLTree {
+
+    class Node {
+    private:
+        Key key;
+        T value;
+        unsigned int balance{};
+        Node *left, *right;
+    public:
+        Node() : key(0), value(0), balance(0), left(nullptr), right(nullptr) {}
+        Node(Key _key, T _value) : key(_key), value(_value) {}
+        explicit Node(Key _key) : key(_key) {}
+        ~Node() { delete left; delete right; }
+    };
+private:
     Node *root;
+public:
+    AVLTree() : root(nullptr) {}
+    explicit AVLTree(Key key) { root = new Node(key); }
+    explicit AVLTree(Key key, T value) { root = new Node(key, value); }
 };
 
 
