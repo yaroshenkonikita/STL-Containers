864ee9015cbf677ab0e148ccefcafc7124a8d4d9
Timur <timkonovalov9858@gmail.com>
2023-02-25 18:28:03 +0300

    list structure init

--- src/SequenceContainer/s21_list.h
+++ src/SequenceContainer/s21_list.h
@@ -1,14 +1,20 @@
-//
-// Created by 12355 on 23.02.2023.
-//
-
 #ifndef CPP2_S21_CONTAINERS_0_SRC_S21_LIST_H
 #define CPP2_S21_CONTAINERS_0_SRC_S21_LIST_H
 
-template<typename T>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+
+template <class T>
 class List {
+ public:
+  using value_type = T;
 
-};
-
+ private:
+  value_type value;
+  List* next_;
+  List* prev_;
+  }; 
 
 #endif //CPP2_S21_CONTAINERS_0_SRC_S21_LIST_H
