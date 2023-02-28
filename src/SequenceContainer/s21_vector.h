7090385ce87374e6a8f05da59463758a3838af47
Timur <timkonovalov9858@gmail.com>
2023-02-28 17:36:55 +0300

    Constructors

--- src/SequenceContainer/s21_vector.h
+++ src/SequenceContainer/s21_vector.h
@@ -8,7 +8,7 @@ namespace s21 {
 template<typename V, class Allocator = std::allocator<V>>
 class vector {
  public:
-//  ------------------------------Member type------------------------------
+  //  ------------------------------Member type------------------------------
   using value_type = V;
   using reference = value_type &;
   using const_reference = const value_type &;
@@ -17,49 +17,78 @@ class vector {
   using size_type = size_t;
   using allocator_type = Allocator;
   using pointer = value_type *;
-//  ------------------------------Functions------------------------------
-  vector() {}
-  vector(size_type n) {}
-  vector(std::initializer_list<value_type> const &items) {}
-  vector(const vector &v) {}
-  vector(vector &&v) {}
-  ~vector() {}
+  //  ------------------------------Functions------------------------------
+  //  default constructor
+  vector() : arr_(nullptr), size_(0), capacity_(0) {}
+  //  parameterized constructor
+  vector(size_type n) : arr_(nullptr), size_(n), capacity_(n) {
+    arr_ = new value_type[size_];
+    for (size_t i = 0; i < size_; i++) {
+      arr_[i] = value_type();
+    }
+  }
+  //  initializer list constructor
+  vector(std::initializer_list<value_type> const &items) : vector(items.size()) {
+    std::copy(items.begin(), items.end(), arr_);
+  }
+  //  copy constructor
+  vector(const vector &v) : vector(v.capacity_) {
+    for (size_type i = 0; i < v.size_; ++i) {
+      arr_[i] = v.arr_[i];
+    }
+    size_ = v.size_;
+    capacity_ = v.capacity_;
+  }
+  //  move constructor
+  vector(vector &&v) noexcept : arr_(v.arr_), size_(v.size_), capacity_(v.capacity_) {
+    v.arr_ = nullptr;
+    v.size_ = 0;
+    v.capacity_ = 0;
+  }
+  //  destructor
+  ~vector() {
+    delete[] arr_;
+    arr_ = nullptr;
+    size_ = 0;
+    capacity_ = 0;
+  }
+  //  overload operator for moving object
   vector &operator=(vector &&v) noexcept {
     if (this != &v) {
-      this->arr_ = v.arr_;
-      this->capacity_ = v.capacity_;
-      this->size_ = v.size_;
+      arr_ = v.arr_;
+      capacity_ = v.capacity_;
+      size_ = v.size_;
       v.arr_ = nullptr;
     }
     return *this;
   }
-//  ------------------------------Element access------------------------------
-//  Возвращает ссылку на элемент в указанном месте pos с проверкой границ
+  //  ------------------------------Element access------------------------------
+  //  Возвращает ссылку на элемент в указанном месте pos с проверкой границ
   reference at(size_type pos) {
     if (pos >= size_) {
       throw std::out_of_range("Out of range");
     }
     return arr_[pos];
   }
-//  Возвращает ссылку на элемент в указанном месте
+  //  Возвращает ссылку на элемент в указанном месте
   reference operator[](size_type pos) { return arr_[pos]; }
-//  Возвращает ссылку на первый элемент в контейнере
+  //  Возвращает ссылку на первый элемент в контейнере
   reference front() { return arr_[0]; }
-//  Возвращает ссылку на последний элемент в контейнере
+  //  Возвращает ссылку на последний элемент в контейнере
   reference back() { return arr_[size_ - 1]; }
-//  Возвращает указатель на базовый массив
+  //  Возвращает указатель на базовый массив
   pointer data() noexcept { return arr_; }
-//  ------------------------------Iterators------------------------------
+  //  ------------------------------Iterators------------------------------
   iterator begin() noexcept {}
   iterator end() noexcept {}
-//  ------------------------------Capacity------------------------------
-//  Проверяет, нет ли в контейнере элементов
+  //  ------------------------------Capacity------------------------------
+  //  Проверяет, нет ли в контейнере элементов
   bool empty() const noexcept { return size_ == 0; }
-//  Возвращает количество элементов в контейнере
+  //  Возвращает количество элементов в контейнере
   size_type size() const noexcept { return size_; }
-//  Возвращает максимальное количество элементов, которые может содержать контейнер
+  //  Возвращает максимальное количество элементов, которые может содержать контейнер
   size_type max_size() const noexcept { return allocator_.max_size(); }
-//  Увеличиваем емкость вектора до значения, которое больше или равно size
+  //  Увеличиваем емкость вектора до значения, которое больше или равно size
   void reserve(size_type size) {
     if (size > max_size()) {
       throw std::length_error("Out of memory");
@@ -73,11 +102,11 @@ class vector {
     capacity_ = buff->capacity_;
     buff = nullptr;
   }
-//  Возвращает количество элементов, для которых в данный момент выделено место в контейнере
+  //  Возвращает количество элементов, для которых в данный момент выделено место в контейнере
   size_type capacity() const noexcept { return capacity_; }
-//  Запрашивает удаление неиспользуемой емкости
+  //  Запрашивает удаление неиспользуемой емкости
   void shrink_to_fit() { if (size_ != capacity_) reserve(size_); }
-//  ------------------------------Modifiers------------------------------
+  //  ------------------------------Modifiers------------------------------
   void clear() noexcept {}
   iterator insert(iterator iter, const_reference value) {}
   void erase(iterator pos) {}
@@ -86,9 +115,9 @@ class vector {
   void swap(vector<value_type> &other) {}
 
  private:
-  size_type size_;
-  size_type capacity_;
   pointer arr_;
+  size_type size_{};
+  size_type capacity_{};
   allocator_type allocator_;
 };
 }
