3d4a199235fae3111d856d8a57ceb30b73959e3c
Nikita Yaroshenko <yaroshenkonikita4@gmail.com>
2023-03-05 19:45:34 +0300

    push

--- src/SortedContainer/BinaryTree.h
+++ src/SortedContainer/BinaryTree.h
@@ -2,89 +2,128 @@
 // Created by 12355 on 24.02.2023.
 //
 
-#ifndef CPP2_S21_CONTAINERS_0_AVLTREE_H
-#define CPP2_S21_CONTAINERS_0_AVLTREE_H
+#ifndef CPP2_S21_CONTAINERS_0_BINARYTREE_H
+#define CPP2_S21_CONTAINERS_0_BINARYTREE_H
 
-// #include <iostream>
+#include <iostream>
+
+#include <memory_resource>
+#include <functional>
+#include <limits>
 
 namespace s21 {
-template <class T, class Compare = std::less<Key>>
-class multiset {
+template <class Key, class Compare = std::less<Key>>
+class BinaryTree {
 public:
-    // using
-    using key_type = T;
-    using value_type = T;
-    using reference = value_type&;
-    using const_reference = const value_type&;
-
-    using size_type = std::size_t;
-    using node_type = Node;
-
-    
-    using iterator = TreeIterator;
-    using const_iterator = TreeConstIterator;
-
-    using allocator_type = std::pmr::polymorphic_allocator<Node>;
-    // using pointer = std::allocator_traits<Allocator>::pointer;
-
+    class TreeIterator;
+    class TreeConstIterator;
+private:
+    // node body ================================================================
     class Node {
-    private:
+    public:
         Node() = default;
-        Node(value_type value) : _value(value) {}
-        Node(value_type value, Node *node) : _value(value), _parent(node) {}
-        Node(Node &node) {
-            _key = std::copy(node._key);
-            _balance = std::copy(node._balance);
-            _right = std::copy(node._right);
-            _left = std::copy(node._left);
-            _parent = std::copy(node._parent);
-        }
-        Node(Node &&node) {
-            _key = std::exchange(node._key, value_type());
-            _balance = std::exchange(node._balance, 0);
-            _right = std::exchange(node._right, nullptr);
-            _left = std::exchange(node._left, nullptr);
-            _parent = std::exchange(node._parent, nullptr);
-        }
-        ~Node() { allocator_type::deallocate(_left, 1); allocator_type::deallocate(_right, 1); }
-        value_type _key{};
+        explicit Node(Key key) : _key(key) {}
+        Node(Key key, Node *node) : _key(key), _parent(node) {}
+        ~Node() { delete _left; delete _right; }
+        Key _key{};
         int _balance{};
         Node *_left{}, *_right{}, *_parent{};
     };
 
-    class Iterator;
-    class ConstIterator;
 
-    node_type *_root{}, *_begin{}, *_end{};
-    size_type _size;
 
-    BinaryTree() { _root = _end = allocator_type::allocate(1); }
-    // BinaryTree(const std::initializer_list<value_type>& items) { _root = new Node(value); }
-    ~BinaryTree() { allocator_type::deallocate(root, 1); }
+public:
+    // pre-implementation =======================================================
 
-    insert() {
-        Node *current_node = _root;
-        if (current_node != _end) {
-        } else {
-            _root = allocator_type::allocate(1);
-            _root._right = current_node;
+    // using ====================================================================
+    using key_type = Key;
+    using value_type = Key;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using node_type = Node;
+    using iterator = TreeIterator;
+    using const_iterator = TreeConstIterator;
+
+
+
+    BinaryTree() { _root = _end = new Node(); }
+    BinaryTree(const std::initializer_list<value_type>& items) {
+         _root = _end = new Node();
+        for (value_type item : items) {
+            insert(item);
         }
+    }
+    ~BinaryTree() { delete _root; }
+
+    void clear() {
+        delete _root;
+        _root = _end = new Node();
+        _begin = nullptr;
+    }
+
+    iterator lower_bound(const_reference key) {
+
+    }
+
+    iterator insert(const_reference key) {
+        Node *current_node = _root;
+        if (current_node == _end) {
+            current_node = _begin = _root = new Node(key);
+            current_node->_right = _end;
+        } else {
+            while (true) {
+                if (Compare{}(key, current_node->_key)) {
+                    if (current_node->_left != nullptr) {
+                        current_node = current_node->_left;
+                    } else {
+                        current_node->_left = new Node(key, current_node);
+                        if (current_node == _begin) {
+                            _begin = current_node->_left;
+                        }
+                        break;
+                    }
+                } else if (current_node->_right != nullptr && current_node->_right != _end) {
+                    current_node = current_node->_right;
+                } else {
+                    if (current_node->_right == _end) {
+                        current_node->_right = new Node(key, current_node);
+                        current_node->_right->_right = _end;
+                    } else {
+                        current_node->_right = new Node(key, current_node);
+                    }
+                    break;
+                }
+            }
+        }
+        ++_size;
     }
     bool empty() {
         return _size == 0;
     }
     size_type max_size() {
-        return std::numeric_limits<size_type>::max() / sizeof(value_type) / 2;
+        return std::numeric_limits<std::size_t>::max() / sizeof(value_type) / 2;
     }
-    class iterator<Node> {
-        Node *iterator_node{};
-    }
+private:
+    node_type *_root{}, *_begin{}, *_end{};
+    size_type _size{};
+};
 
-//    BinaryTree *a = new BinaryTree<int>{1};
+// iterators ================================================================
+template <typename Key, typename Compare>
+class BinaryTree<Key, Compare>::TreeIterator {
+public:
+private:
+
+};
+
+template <typename Key, typename Compare>
+class BinaryTree<Key, Compare>::TreeConstIterator {
+
 };
 
 } // namespace s21
 
-
-//std::numeric_limits<type>::max()
-#endif //CPP2_S21_CONTAINERS_0_AVLTREE_H
+#endif //CPP2_S21_CONTAINERS_0_BINARYTREE_H
---
+++ src/SortedContainer/main.cc
@@ -0,0 +1,10 @@
+//
+// Created by 12355 on 05.03.2023.
+//
+
+#include "BinaryTree.h"
+
+int main() {
+    s21::BinaryTree<int> asd{1, 2, 3, 4};
+    return 0;
+}
