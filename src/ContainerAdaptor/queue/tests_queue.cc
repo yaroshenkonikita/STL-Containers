29cf65885f3496be8fba1f6e756a039b2d59e42d
Polliver Helene <polliver@at-n3.msk.21-school.ru>
2023-03-11 10:29:35 +0300

    queue is ready - protected from list

--- src/ContainerAdaptor/queue/tests_queue.cc
+++ src/ContainerAdaptor/queue/tests_queue.cc
@@ -1,6 +1,74 @@
 #include <gtest/gtest.h>
 
 #include "s21_queue.h"
+#include <queue>
+
+TEST(queue_tests, def_constructor) {
+  s21::queue<int> basic;
+  EXPECT_ANY_THROW(basic.front());
+  EXPECT_ANY_THROW(basic.back());
+  EXPECT_TRUE(basic.empty());
+}
+
+TEST(queue_tests, parametr_constructor) {
+  s21::queue<int> basic(12);
+  EXPECT_EQ(basic.front(), int());
+  EXPECT_EQ(basic.back(), int());
+  EXPECT_EQ(basic.size(), size_t(12));
+  EXPECT_FALSE(basic.empty());
+}
+
+TEST(queue_tests, parametr_constructor2) {
+  s21::queue<std::string> basic(12);
+  EXPECT_EQ(basic.front(), std::string());
+  EXPECT_EQ(basic.back(), std::string());
+  EXPECT_EQ(basic.size(), size_t(12));
+  EXPECT_FALSE(basic.empty());
+}
+
+TEST(queue_tests, initializer_list_constructor) {
+  s21::queue<int> basic({1, 2, 3, 4, 5});
+  EXPECT_EQ(basic.front(), 1);
+  EXPECT_EQ(basic.back(), 5);
+  EXPECT_EQ(basic.size(), size_t(5));
+  EXPECT_FALSE(basic.empty());
+}
+
+TEST(queue_tests, initializer_list_constructor2) {
+  s21::queue<std::string> basic({"hello", "world"});
+  EXPECT_TRUE(basic.size() == 2);
+  EXPECT_EQ(basic.back(), "world");
+  EXPECT_EQ(basic.front(), "hello");
+}
+
+TEST(queue_tests, initializer_list_constructor3) {
+  std::string element = "hello";
+  std::string element2 = "world";
+  std::string element3 = "!!!";
+  std::string element4 = "&";
+  s21::queue<std::string> basic({element, element2, element3, element4});
+  EXPECT_TRUE(basic.size() == 4);
+  EXPECT_EQ(basic.back(), element4);
+  EXPECT_EQ(basic.front(), element);
+}
+
+TEST(queue_tests, copy_constructor) {
+  std::string element = "hello";
+  std::string element2 = "world";
+  std::string element3 = "!!!";
+  std::string element4 = "&";
+  s21::queue<std::string> basic({element, element2, element3, element4});
+  s21::queue<std::string> basic2(basic);
+  EXPECT_TRUE(basic2.size() == 4);
+  EXPECT_EQ(basic2.back(), element4);
+  EXPECT_EQ(basic2.front(), element);
+}
+
+TEST(queue_tests, copy_constructor2) {
+  s21::queue<std::string> basic;
+  s21::queue<std::string> basic2(basic);
+  EXPECT_TRUE(basic2.empty());
+}
 
 TEST(queue_tests, push1) {
   s21::queue<int> basic;
@@ -26,7 +94,7 @@ TEST(queue_tests, push_pop) {
 }
 
 TEST(queue_tests, push_pop2) {
-  std::queue<int> basic;
+  s21::queue<int> basic;
   int element = 5;
   int element2 = 4;
   basic.push(element);
@@ -35,6 +103,28 @@ TEST(queue_tests, push_pop2) {
   basic.pop();
   EXPECT_EQ(basic.back(), element2);
   EXPECT_EQ(basic.front(), element2);
+}
+
+TEST(queue_tests, push_pop3) {
+  s21::queue<std::string> basic;
+  std::string element = "hello";
+  std::string element2 = "world";
+  basic.push(element);
+  basic.push(element2);
+  basic.push(element2);
+  basic.pop();
+  EXPECT_EQ(basic.back(), element2);
+  EXPECT_EQ(basic.front(), element2);
+}
+
+TEST(queue_tests, swap) {
+  s21::queue<std::string> basic({"hi", "ho"});
+  EXPECT_TRUE(!basic.empty());
+  s21::queue<std::string> basic2;
+  basic.swap(basic2);
+  EXPECT_EQ(basic2.back(), "ho");
+  EXPECT_EQ(basic2.front(), "hi");
+  EXPECT_TRUE(basic.empty());
 }
 
 int main(int argc, char *argv[]) {
