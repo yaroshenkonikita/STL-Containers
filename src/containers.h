ac0c5d70598f0d3a3c6d8af870ce0961af851d30
tokimikichika <yaroshenkonikita4@gmail.com>
2023-10-09 20:48:50 +0300

    rework basic headers

---
+++ src/containers.h
@@ -0,0 +1,11 @@
+#ifndef STL_CONTAINERS_CONTAINERS_H_
+#define STL_CONTAINERS_CONTAINERS_H_ 1
+
+#include "containers/list.h"
+#include "containers/map.h"
+#include "containers/queue.h"
+#include "containers/set.h"
+#include "containers/stack.h"
+#include "containers/vector.h"
+
+#endif  // STL_CONTAINERS_CONTAINERS_H_
